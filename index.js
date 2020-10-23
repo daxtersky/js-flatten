95e9127b2bc577e70a2bf0afa7ed1d8c0f1fac5f
daxtersky <daxtersky@gmail.com>
2020-10-23 15:34:12 +0200

    1 file modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
-// Import stylesheets
-import "./style.css";
+import { nestedArray } from "./data";
 
-// Write Javascript code!
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>Flatten multidimentional arrays</h1>`;
 
-//non recursive flatten deep using a stack
-var arr1 = [1, 2, 3, [1, 2, 3, 4, [5, 6, [7, 8]]]];
-console.log("A 0", arr1);
-function flatten(input) {
-  const stack = [...input];
-  const res = [];
-  while (stack.length) {
-    // pop value from stack
-    const next = stack.pop();
-    if (Array.isArray(next)) {
-      // push back array items, won't modify the original input
-      stack.push(...next);
-    } else {
-      res.push(next);
+console.log("nestedArray", nestedArray);
+
+const createFlattenMenuItemList = menuItems => {
+  const flatten = [];
+  for (const menuItem of menuItems) {
+    if (menuItem.formId) {
+      flatten.push(menuItem);
+    }
+    addMenuItemChildren(menuItem, flatten);
+  }
+  return flatten;
+};
+
+const addMenuItemChildren = (menuItem, flattenArray) => {
+  if (menuItem.subMenuItemList?.length > 0) {
+    for (const subMenuItem of menuItem.subMenuItemList) {
+      flattenArray.push(subMenuItem);
+      const moreChildren = addMenuItemChildren(subMenuItem, flattenArray);
+      if (moreChildren) {
+        return moreChildren;
+      }
     }
   }
-  //reverse to restore input order
-  return res.reverse();
-}
-flatten(arr1); // [1, 2, 3, 1, 2, 3, 4, 2, 3, 4]
-console.log("A 1", flatten(arr1));
+};
 
-//recursive flatten deep
-function flatten(array) {
-  var flattend = [];
-  !(function flat(array) {
-    array.forEach(function(el) {
-      if (Array.isArray(el)) flat(el);
-      else flattend.push(el);
-    });
-  })(array);
-  return flattend;
-}
-flatten(arr1);
-console.log("B 1", flatten(arr1));
+const flattenArray = createFlattenMenuItemList(nestedArray);
+
+console.log("flattenArray", flattenArray);
